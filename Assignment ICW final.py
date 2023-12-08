23a33a95656cadc4ba53132ce4316846cda6338a
Senal2007 <152157532+Senal2007@users.noreply.github.com>
2023-12-08 09:13:50 +0530

    Add files via upload

--- Assignment ICW final.py
+++ Assignment ICW final.py
@@ -1,5 +1,19 @@
 #Main interface (20231282-S.S.Rajendra)
+
+ongoing = [[] for _ in range(25)]
+onhold = [[] for _ in range(25)]
+completed = [[] for _ in range(25)]
+
+#List for the data entries
+projectEntry = []
+
+ongoingProjects = []
+onholdProjects = []
+completedProjects = []
+
 # Initialize variables
+workers = 0
+code = 0
 
 def add_new_project():
   print("Add a new project to existing projects.")
@@ -23,12 +37,13 @@ def main():
   # Display the main menu panel.
   print("----------------XYZ Company----------------")
   print("                 Main Menu                 ")
-  print("\n   1) Add a new project to existing projects.")
-  print("   2) Remove a completed project from existing projects.")
-  print("   3) Add new workers to available workers group.")
-  print("   4) Update details on ongoing projects.")
-  print("   5) Project Statistics")
-  print("   6) Exit")
+  print("\n1) Add a new project to existing projects")
+  print("2) Remove a completed project from existing projects")
+  print("3) Add new workers to available workers group")
+  print("4) Update details on ongoing projects")
+  print("5) Project Statistics")
+  print("6) Exit")
+
 
   # Identify User's choice.
   choice = input("                                        Your Choice: ")
@@ -47,61 +62,145 @@ def main():
   elif choice == "6":
     exit_program()
   else:
-    # choice selected is invalid
-    print("choice selected is invalid")
+      main()
 
   # 2nd figure ICW program
 
-def add_new_project():
+def add_new_project():  # sourcery skip: remove-unreachable-code
   # Get the new project details from the user.
   print("\n----------------XYZ company----------------")
   print("             Add a new project               ")
-  project_code = int(input("\nproject code: "))
-  if project_code == 0:
+  code = int(input("\nproject code: "))
+  if code == 0:
     return main()
+  if code > 0:
+    projectEntry.append(code)
+  else:
+    add_new_project()
+
   client_name = str(input("\nclient name: "))
-  from datetime import date 
+  projectEntry.append(client_name)
+
   start_date = input("start date (DD/MM/YYYY): ")
+  projectEntry.append(start_date)
+
   expected_end_date = input("expected end date (DD/MM/YYYY): ")
+  projectEntry.append(expected_end_date)
+
   number_of_workers = int(input("number of workers: "))
+  projectEntry.append(number_of_workers)
+
+  #     if numOfWorkers > 0 and numOfWorkers <= workers: 
+#         workers = workers - numOfWorkers
+#         projectEntry.append(numOfWorkers)
+#     elif numOfWorkers > 0 and numOfWorkers > workers:
+#         os.system("cls")
+#         print()
+#         print(""" ---------------------XYZ Company---------------------
+              
+#   > Not enough workers to start the project.""")
+#         input()
+#         menu()
+#     else:
+#         print("Invalid input!")
+#         input()
+#         addProject()
+
   project_status = str(input("project status (ongoing, on hold, or completed): "))
+  projectEntry.append(project_status)
+  
   if (project_status == "completed"):
      input("Actual end date (DD/MM/YYYY): ")
+
   # Prompt the user to save the project
-  Do_you_want_to_save_the_project = str(input("\nDo you want to save the project (yes/no)?: "))
-  # Save the new project if user wants to.
-  if (Do_you_want_to_save_the_project == "yes"):
-     print("The new project has been added.")
-  else :
-     print("The new project has not been added.")
+  Save_project = str(input("\nDo you want to save the project (Y/N)?: "))
   
+  #save the project if user wants to.
+  if Save_project == "Y":
+    
+   if project_status == ("ongoing"):
+    ongoing[code] = projectEntry
+    ongoingProjects.append(code)
+    
+   elif project_status == ("onhold"):
+    onhold[code] = projectEntry
+    onholdProjects.append(code)
+
+   elif project_status == ("completed"):
+    completed[code] = projectEntry
+    completedProjects.append(code)
+
+   print("The new project has been added.")
+   return main()
+
+  if Save_project == ("N"):
+   print("An error occured!")
+   return main()
+    
    # 3rd figure ICW program
 
+
 def remove_completed_project():
-    # Get the existing project details from the user.
-    print("\n----------------XYZ company----------------")
-    print("          Remove Completed Project         ")
-    project_code = int(input("\nproject code: "))
-    Do_you_want_to_remove_the_project = str(input("do you want to remove the project (yes/no)?: "))
-    # Remove the existing project if user wants to.
-    if (Do_you_want_to_remove_the_project == "yes"):
-        print("The existing project has been removed.")
-    else:
-       print("The project has not been removed.")
+  # Get the existing project details from the user.
+  print("\n----------------XYZ company----------------")
+  print("          Remove Completed Project         ")
+  project_code = int(input("\nproject code: "))
+  
+  if project_code in ongoingProjects:
+    print(ongoing)
+    ongoing[project_code]=[]
+    del(ongoingProjects[code])
+    print
+    input(completed)
+    return main()
+  
+  elif project_code in onholdProjects:
+    print(onhold)
+    onhold[project_code]=[]
+    del(onholdProjects[code])
+    print
+    input(onhold)
+    return main()
+  else:
+    print("Invalid input!")
+    input()
+    return remove_completed_project()
+  
+  # Remove the existing project if user wants to.
+  
+  removethe_project = str(input("Do you want to remove the project (Y/N)?:"))
+  if (removethe_project == "Y"):
+      print("The existing project has been removed.")
+  else:
+     print("The project has not been removed.")
+     return main()
    
     # 4th figure ICW program
 
 def add_new_workers():
     # Identify how many workers to add.
+    workers = 0
     print("\n----------------XYZ company----------------")
     print("              Add new workers              ")
-    number_workers_to_add = int(input("\nnumber of workers to add: "))
-    # Prompt the user to add workers or not.
-    Do_you_want_to_add = str(input("do you want to add (yes/no)?: "))
-    # Add the new workers if the user wants to.
-    if (Do_you_want_to_add == "yes"):
-        print("New workers have been added.")
+    workersto_add = int(input("\nnumber of workers to add ('0' to exit): "))
+    if workersto_add == "0":
+     return main()
+    elif workers():
+     workers += workers
+     print("Workers added succesfully")
+     input()
+     return main()
     else:
+     print ("Invalid input")
+     input()
+     add_new_workers
+     return workersto_add
+    # Prompt the user to add workers or not.
+    wantto_add = str(input("do you want to add (Y/N)?: "))
+    # Add the new workers if the user wants to.
+    if (wantto_add == "Y"):
+        print("New workers have been added.")
+    elif (wantto_add == "N"):
        print("New workers have not been added.")
 
     # 5th figure ICW program
@@ -110,20 +209,24 @@ def update_ongoing_projects():
     # Ask which project needs to be updated.
     print("\n----------------XYZ company----------------")
     print("           Update Project details          ")
-    Project_update_code = int(input("\nEnter project code: "))
+    Project_update_code = int(input("\nEnter project code (1-25) '0' to exit: "))
     if Project_update_code == 0:
        main()
+    if Project_update_code in ongoingProjects:
+      ongoing[Project_update_code][0] = Project_update_code
+    elif Project_update_code in onholdProjects:
+      onhold[Project_update_code][0] = Project_update_code
+    else:
+      print ("Project not found!")
+      return main()
+    
     Client_name = str(input("\nClient name: "))
-    Start_date = str(input("start date (DD/MM/YYYY): "))
-    Expected_end_date = str(input("expected end date (DD/MM/YYYY): "))
-    Number_of_workers = int(input("number of workers: "))
-    project_status_2 = str(input("project status (ongoing, on hold, or complete): "))
-    if (project_status_2 == "completed"):
-      input("Actual end date (DD/MM/YYYY): ")
+    
+    
     # Prompt the user to update the project details or not.
-    Do_you_want_to_update_details = str(input("\ndo you want to update project details (yes/no)?: "))
+    Do_you_want_to_update_details = str(input("\nDo you want to update project details (Y/N)?: "))
     # Update the ongoing project if the user wants to.
-    if (Do_you_want_to_update_details == "yes") :
+    if (Do_you_want_to_update_details == "Y"):
         print("The project has been updated.")
     else:
         print("The project is not updated.")
@@ -134,35 +237,21 @@ def project_statistics():
     #Get the project statistics from user.
     print("\n----------------XYZ company----------------")
     print("               Project statistics            ")
-    number_of_ongoing_projects = int(input("\nenter the num of ongoing projects: "))
-    number_of_on_hold_projects = int(input("enter the number of on hold projects: "))
-    number_of_available_workers_to_assign = int(input("number of workers available: "))
+    #print the stats
+    print(f"\nNumber of ongoing projects: {len(ongoingProjects)}")
+    print(f"Number of on-hold projects: {len(onholdProjects)}")
+    print(f"Number of complete projects: {len(completedProjects)}")
+    print(f"Number of available workers: {workers}")
     # saving the project
-    do_you_want_to_add_the_project = str(input("\ndo you want to add the project (yes/no)?:"))
-    # Ask if the user wants to update project stats.
-    if (do_you_want_to_add_the_project == "yes"):
-        print("project statistics have been updated.") 
-    else:
-        print("project statistics have not been updated.")
+    exit_gate = str(input("\nEnter '0' to exit:"))
+    if exit_gate == '0':
+      return main()
+
 
 def exit_program():
     print("      Exiting the program...")
+    return main()
      
 
 if __name__ == "__main__":
   main()
-
-                                 
-                                     
-    
-    
-    
-        
-        
-
-    
-    
-
-
-
-    
